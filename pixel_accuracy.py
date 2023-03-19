9b604d92b9b16399e03c115322778e49dbac20d0
Krupal Prajapati <68751988+space-beep@users.noreply.github.com>
2023-03-19 07:56:05 +0100

    Update pixel_accuracy.py

--- pixel_accuracy.py
+++ pixel_accuracy.py
@@ -1,8 +1,6 @@
 import numpy as np
 from PIL import Image
-from precision_score import precision
-from recall_score import recall
-from jaccard_index import jaccard_index
+
 
 def pixel_accuracy(pred, target):
     """
@@ -30,13 +28,4 @@ def pixel_accuracy(pred, target):
     
 accuracy = pixel_accuracy('predicted.png', 'original.png')
 
-jaccard = jaccard_index('predicted.png', 'original.png')
-precision = precision('predicted.png', 'original.png')
-recall = recall('predicted.png', 'original.png')
-
-
-print("Jaccard Index:", jaccard)
-print("Pixel accuracy:", accuracy)
-print("Precision :",precision)
-print("Recall score :",recall)
 
